e12fa659382ca49c6289bd03ee020667d5864865
Anoop M D <anoop.md1421@gmail.com>
2021-03-19 18:14:25 +0530

    Added Readme ;)

--- src/main/java/com/example/demo/student/StudentRepository.java
+++ src/main/java/com/example/demo/student/StudentRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.nio.file.OpenOption;
 import java.util.Optional;
 
 @Repository
